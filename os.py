8a5af9b4b07d5435c266bafb8aa1f750e1ce7f31
MarkPrabhu4 <markprabhubilla@gmail.com>
2025-06-09 01:31:57 +0530

    Update os.py

--- os.py
+++ os.py
@@ -1,8 +1,1 @@
-import os
 
-print("Current working directory:", os.getcwd())
-
-if os.path.isfile("chord001.wav.mp3"):
-    print("✔️ File found in current directory.")
-else:
-    print("❌ File NOT found in current directory.")
